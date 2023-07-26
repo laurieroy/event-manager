5c520375c2e6b6ffeb426203fce1ccb3e85bcdba
Laurie Roy <laurieroy.dev@gmail.com>
2023-07-26 16:53:58 -0500

    Add Event

--- app/javascript/components/Editor.js
+++ app/javascript/components/Editor.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import react, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Header from "./Header";
--- app/javascript/components/EventList.js
+++ app/javascript/components/EventList.js
@@ -1,3 +1,4 @@
+import React from "react";
 import PropTypes from "prop-types";
 
 const EventList = ({ events }) => {
@@ -6,9 +7,11 @@ const EventList = ({ events }) => {
 
     return eventArray.map((event) => {
       <li key={event.id}>
-        {event.event_date}
-        {" - "}
-        {event.event_type}
+        <Link to={`/events/${event.id}`}>
+          {event.event_date}
+          {" - "}
+          {event.event_type}
+        </Link>
       </li>;
     });
   };
